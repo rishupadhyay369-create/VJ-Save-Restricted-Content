6880f66add997e2b53401aea1f8484327b9aa196
rishupadhyay369-create <rishupadhyay369@gmail.com>
2025-12-30 12:18:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,43 +1,42 @@
+
 # Don't Remove Credit Tg - @VJ_Bots
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
 import os
 
-# Login feature
-LOGIN_SYSTEM = os.environ.get("LOGIN_SYSTEM", "True").lower() == "true"
+# Login system
+LOGIN_SYSTEM = True
 
-if LOGIN_SYSTEM is False:
-    STRING_SESSION = os.environ.get("STRING_SESSION", "")
+if not LOGIN_SYSTEM:
+    STRING_SESSION = ""
 else:
     STRING_SESSION = None
 
 # Bot token
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = "PASTE_YOUR_BOT_TOKEN_HERE"
 
 # API ID
-API_ID = int(os.environ.get("API_ID", "30720387"))
+API_ID = 30720387
 
 # API HASH
-API_HASH = os.environ.get(
-    "API_HASH",
-    "2eeb9d06eb049619e747488846496bfa"
-)
+API_HASH = "2eeb9d06eb049619e747488846496bfa"
 
-# Admin ID
-ADMINS = int(os.environ.get("ADMINS", "6690830189"))
+# Admin ID (must be LIST)
+ADMINS = [6690830189]
 
 # Channel ID
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003659159660"))
+CHANNEL_ID = -1003659159660
 
 # MongoDB
-DB_URI = os.environ.get(
-    "DB_URI",
-    "mongodb+srv://rishupadhyay369_db_user:Kt4Q2zYLs3UrAXUh@cluster0.dftynvv.mongodb.net/?appName=Cluster0"
+DB_URI = (
+    "mongodb+srv://rishupadhyay369_db_user:"
+    "Kt4Q2zYLs3UrAXUh@cluster0.dftynvv.mongodb.net/"
+    "?appName=Cluster0"
 )
 
 # Flood wait
-WAITING_TIME = int(os.environ.get("WAITING_TIME", "10"))
+WAITING_TIME = 10
 
 # Error message
-ERROR_MESSAGE = os.environ.get("ERROR_MESSAGE", "True").lower() == "true"
+ERROR_MESSAGE = True
