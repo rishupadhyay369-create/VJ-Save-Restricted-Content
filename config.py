d16bc1cb6678c6fa7da39937b432d0c09c022543
rishupadhyay369-create <rishupadhyay369@gmail.com>
2025-12-30 12:33:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,27 @@
-
 # Don't Remove Credit Tg - @VJ_Bots
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
 
 import os
+import threading
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+# -------------------- KOYEB HEALTH SERVER --------------------
+
+def run_server():
+    class HealthHandler(BaseHTTPRequestHandler):
+        def do_GET(self):
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"Bot is alive")
+
+    port = int(os.environ.get("PORT", 8000))
+    server = HTTPServer(("0.0.0.0", port), HealthHandler)
+    server.serve_forever()
+
+threading.Thread(target=run_server, daemon=True).start()
+
+# -------------------- BOT CONFIG --------------------
 
 # Login system
 LOGIN_SYSTEM = True
@@ -13,30 +31,28 @@ if not LOGIN_SYSTEM:
 else:
     STRING_SESSION = None
 
-# Bot token
-BOT_TOKEN = "PASTE_YOUR_BOT_TOKEN_HERE"
+# Bot token (YOU MUST FILL THIS)
+BOT_TOKEN = "PASTE_YOUR_REAL_BOT_TOKEN_HERE"
 
-# API ID
+# API credentials (FILLED)
 API_ID = 30720387
-
-# API HASH
 API_HASH = "2eeb9d06eb049619e747488846496bfa"
 
-# Admin ID (must be LIST)
+# Admin ID (FILLED AS LIST)
 ADMINS = [6690830189]
 
-# Channel ID
+# Channel ID (FILLED)
 CHANNEL_ID = -1003659159660
 
-# MongoDB
+# MongoDB URI (FILLED)
 DB_URI = (
     "mongodb+srv://rishupadhyay369_db_user:"
     "Kt4Q2zYLs3UrAXUh@cluster0.dftynvv.mongodb.net/"
     "?appName=Cluster0"
 )
 
-# Flood wait
+# Flood wait time
 WAITING_TIME = 10
 
-# Error message
+# Error message toggle
 ERROR_MESSAGE = True
